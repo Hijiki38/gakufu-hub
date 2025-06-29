6ba25b3081b75e5c91ea9c6c025d752ff0200cc2
堀田拓海 <takumimh@gmail.com>
2025-06-29 23:24:33 +0900

    Add access control to storage definition for guest and authenticated users

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -2,4 +2,10 @@ import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
   name: 'GakufuHubStorage',
+  access: (allow) => ({
+    'data/*': [
+      allow.guest.to(['read']),
+      allow.authenticated.to(['read', 'write', 'delete']),
+    ]
+  })
 });
