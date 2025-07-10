427d8564de0076a9a25c5442646299566d0f57a5
堀田拓海 <takumimh@gmail.com>
2025-07-11 00:03:51 +0900

    refactor: simplify Amplify configuration and update storage access permissions

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -4,7 +4,7 @@ export const storage = defineStorage({
   name: 'GakufuHubStorage',
   access: (allow) => ({
     'data/*': [
-      allow.guest.to(['read']),
+      allow.guest.to(['read', 'write']),
       allow.authenticated.to(['read', 'write', 'delete']),
     ]
   })
